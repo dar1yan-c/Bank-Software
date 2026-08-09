1262be83082964e15ff03e152c8247569af86e3d
Dari-Laptop <dariyanchochev@gmail.com>
2026-08-09 21:59:54 +0300

    Added the post request from login.js

--- js/login.js
+++ js/login.js
@@ -2,3 +2,29 @@ const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const button = document.querySelector("button");
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("Handler fired!");
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: nameInput.value,
+            email: emailInput.value,
+            password: passwordInput.value,
+        }),
+    })
+        .then((response) => response.json())
+        .then((data) => console.log("Success:", data))
+        .catch((err) => console.error("Error:", err));
+
+    if (!response.ok) {
+        throw new Error;
+    }
+    else {
+
+    }
+});
